6e03df168a51b89aa0b83e0a7d31d5624664efe3
Nick Wichert <nick.wichert@icloud.com>
2018-12-27 23:11:51 -0600

    Initial Commit

---
+++ ARKitCarGeolocation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARKitCarGeolocation
+//
+//  Created by Nick Wichert on 12/27/18.
+//  Copyright © 2018 Nick Wichert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
